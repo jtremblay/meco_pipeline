dce94608cf1c322927a4f8b64d255c104b8127d4
jtremblay <jtrembla@gra-login3.graham.sharcnet>
2024-08-21 09:55:07 -0400

    fix

--- bio/shotgun_metagenomics.py
+++ bio/shotgun_metagenomics.py
@@ -2337,7 +2337,7 @@ def edger_glm(abundance, mapping_file, outdir):
     )
     
     job.command="""
-edgerFeaturesGLM.R \\
+edgerFeaturesGLM_SMG.R \\
   -i {abundance} \\
   -o {outdir} \\
   -m {mapping_file} \\
